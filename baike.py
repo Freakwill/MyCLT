efde9e7fa99ecc64149bfc72965f832bb743c30f
William Song <30965609+Freakwill@users.noreply.github.com>
2018-06-04 11:05:33 +0800

    Add files via upload
    get information from baidu baike

---
+++ baike.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from bs4 import BeautifulSoup
+import requests
+
+BAIKE_URL = 'https://baike.baidu.com'
+ITEM_URL = BAIKE_URL + '/item'
+
+parser = argparse.ArgumentParser(description='WeChat')
+parser.add_argument('-i', dest='item', action='store', default='百度百科', metavar='ITEM')
+args = parser.parse_args()
+
+url = ITEM_URL + '/%s'%args.item
+header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) Gecko/20100101 Firefox/57.0'}
+resp = requests.get(url, headers=header_dict)
+bs = BeautifulSoup(resp.content.decode('utf-8'), "lxml")
+summary = bs.find('div', {'class':'lemma-summary'})
+print(summary.text)
