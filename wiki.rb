1087980245df2a0b1d244d36cd1dceb6ae29929d
William Song <30965609+Freakwill@users.noreply.github.com>
2019-02-13 20:11:50 +0800

    Add files via upload

---
+++ wiki.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+=begin
+get info of items from wikipedia
+
+python wiki.rb -i [any item]
+
+require wikipedia, thor
+=end
+
+require 'wikipedia'
+
+require "thor"
+ 
+class WikiCLI < Thor
+    # ruby wiki.rb find --item Python
+    desc "Wiki", "Ruby cli for Wikipedia"
+    option :item, :aliases => :i
+    def find
+        page = Wikipedia.find(n=options[:item])
+        puts page.summary
+    end
+end
+ 
+WikiCLI.start(ARGV)
