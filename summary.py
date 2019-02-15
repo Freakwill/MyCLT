21464a4e35c8cc57f667bbfedd36b43686336d88
William Song <30965609+Freakwill@users.noreply.github.com>
2019-02-15 12:16:10 +0800

    Add files via upload

--- summary.py
+++ summary.py
@@ -7,12 +7,26 @@ logging.propagate = False
 logging.getLogger().setLevel(logging.ERROR)
 
 import argparse
-import pdfsummary
+import pathlib
+
 
 parser = argparse.ArgumentParser(description='Summarize a pdf')
-parser.add_argument('-p', dest='path', action='store', help='the path of the file')
+parser.add_argument('-p', dest='path', action='store', type=pathlib.Path, help='the path of the file')
 parser.add_argument('-n', dest='count', action='store', default=10, type=int, help='the number of sentences')
+parser.add_argument('-l', dest='language', action='store', default='english', help='the language used')
 args = parser.parse_args()
 
+import pdfsummary
 
-pdf.summary.show_summary(args.path, args.count)
+
+if args.path.suffix == '.pdf':
+    pdfsummary.show_summary(args.path, args.count)
+else:
+    def show_summary(path, *args, **kwargs):
+        print('Summary of %s' % path)
+        with open(path, 'rb') as fo:
+            s = fo.read()
+        pdfsummary.show(pdfsummary.summarize(s, *args, **kwargs))
+
+    show_summary(args.path, args.count, args.language)
+
